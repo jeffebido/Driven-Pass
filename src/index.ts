679f468da9aff7cd2334689110e6f0f35a9c0251
Jefferson Oliveira <jeffebido@hotmail.com>
2022-12-12 16:36:53 -0300

    credential testes thunder client

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import authRouter from './routes/authRouter';
+import credentialRouter from './routes/credentialRouter';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(authRouter);
+app.use(credentialRouter);
 
 const PORT = process.env.PORT || '5000';
 const HOST = '127.0.0.1';
